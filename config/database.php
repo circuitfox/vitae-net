94378bf1e851ce8f960e3f3cf03c4731cc1518be
Chris Stankus <cdstankus@gmail.com>
2017-11-01 21:35:21 -0500

    Migration configuration for deployment

--- config/database.php
+++ config/database.php
@@ -54,6 +54,21 @@ return [
             'engine' => null,
         ],
 
+        'migrate' => [
+            'driver' => 'mysql',
+            'host' => env('MIGRATE_HOST', '127.0.0.1'),
+            'port' => env('MIGRATE_PORT', '3306'),
+            'database' => env('MIGRATE_DATABASE', 'forge'),
+            'username' => env('MIGRATE_USERNAME', 'forge'),
+            'password' => env('MIGRATE_PASSWORD', ''),
+            'unix_socket' => env('MIGRATE_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
--- scripts/after-install-www.sh
+++ scripts/after-install-www.sh
@@ -6,7 +6,7 @@ composer install --quiet --no-ansi --no-dev --no-interaction --no-scripts --no-p
 
 cp /etc/medscanner/env .env
 php artisan key:generate
-php artisan migrate
+php artisan migrate --database=migrate
 
 npm install
 npm run dev
