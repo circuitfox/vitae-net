c216c37f16379a411e6c00a4736637160ceb508c
Chris Stankus <cdstankus@gmail.com>
2017-11-01 22:35:28 -0500

    Prevent instaling into old directory

--- scripts/after-install-root.sh
+++ scripts/after-install-root.sh
@@ -5,5 +5,6 @@ cp aws/config/medscanner-nginx /etc/nginx/sites-enabled/medscanner
 ln -s -f /etc/nginx/sites-enabled/medscanner /etc/nginx/sites-available/medscanner
 
 sed -i 's/;cgi_fixpathinfo=1/cgi_fixpathinfo=0/' /etc/php/7.0/fpm/php.ini
+rm -rf /var/www/medscanner
 cp -r /home/ubuntu/medscanner /var/www/medscanner
 chown -R www-data:www-data /var/www/medscanner
