b1b9c536ca4a93fb66de5504ae72db9d659c4078
Chris Stankus <cdstankus@gmail.com>
2017-11-01 22:26:40 -0500

    Fix bad nginx config symlinks

--- scripts/after-install-root.sh
+++ scripts/after-install-root.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 cd /home/ubuntu/medscanner
 
-cp aws/config/medscanner-nginx /etc/nginx/sites-available/medscanner
-if [ ! -f /etc/nginx/sites-enabled/medscanner ]; then
-    ln -s /etc/nginx/sites-available/medscanner /etc/nginx/sites-enabled/medscanner
-fi
+cp aws/config/medscanner-nginx /etc/nginx/sites-enabled/medscanner
+ln -s -f /etc/nginx/sites-enabled/medscanner /etc/nginx/sites-available/medscanner
 
 sed -i 's/;cgi_fixpathinfo=1/cgi_fixpathinfo=0/' /etc/php/7.0/fpm/php.ini
 cp -r /home/ubuntu/medscanner /var/www/medscanner
