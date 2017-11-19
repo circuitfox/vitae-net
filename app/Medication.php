4452a9a97af30d5672ecf1904539ce489cd1f424
Carl <carlgoshert@gmail.com>
2017-11-19 13:41:28 -0600

    Forgot to remove incrementing=false when changed key to bigIncrement

--- app/Medication.php
+++ app/Medication.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Medication extends Model
 {
-    public $incrementing = false;
     protected $fillable = [
       'medication_id', 'name', 'dosage_amount', 'dosage_unit',
       'instructions', 'comments', 'stat',
