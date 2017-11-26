ebed1d0067022f52541c3d07855b0bb24662bff7
Chris Stankus <cdstankus@gmail.com>
2017-11-26 00:09:04 -0600

    views/admin.patients.create: add test

---
+++ tests/Feature/PatientsCreatePageTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class PatientsCreatePageTest extends TestCase
+{
+    public function testHasPatientForm()
+    {
+        $user = factory(\App\User::class)->states('admin')->create();
+        $response = $this->actingAs($user)->get('/patients/create');
+        $response->assertSee('<patient-form :errors=')->assertSee('></patient-form');
+    }
+}
