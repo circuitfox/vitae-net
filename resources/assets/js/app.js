6af69c7f7ec71574c6da8cb33b972fb0348e48c3
Carl <carlgoshert@gmail.com>
2017-12-05 09:24:21 -0600

    Cleanup issues from rebase.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -95,15 +95,6 @@ $(() => {
             }
         }
     });
-    // FIXME: Scanning library setup & events
-    // FIXME: find a better way to test this
-    console.log("patient emit");
-    summaryPage.$emit('set-patient', {first_name: 'George', last_name: 'Smith', dob: '1/9/1993', mrn: 605065, sex: 'Male', physician: 'Dr. Jones', room: '12'});
-    console.log("medication emit");
-    summaryPage.$emit('add-medication', {name: 'Wellbutrin', dosage: 100, units: 'mg', instructions: '1 pill by mouth ever 4 hours', comments: '', stat: false});
-    $('#form-extra').show();
-
-    addPatientPage.$emit('set-patient', {first_name: 'George', last_name: 'Smith', dob: '1/9/1993', mrn: 605065});
 
     $('#add-medication').on('click', () => {
         medicationForm.$emit('add-medication', {name: '', dosage_amount: 0, dosage_unit: ''});
@@ -118,7 +109,6 @@ $(() => {
 
     //addPatientPage.$emit('set-patient', {first_name: 'George', last_name: 'Smith', dob: '1/9/1993', mrn: 605065});
     //medicationForm.$emit('add-medication', {name: 'Wellbutrin', dosage_amount: 100, dosage_unit: 'mg'});
-    medicationForm.$emit('add-medication', {name: 'Wellbutrin', dosage_amount: 100, dosage_unit: 'mg'});
 
     $('#user-delete-modal').on('show.bs.modal', function (event) {
         var button = $(event.relatedTarget);
