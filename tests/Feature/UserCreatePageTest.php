b260acc85b6a67db023a0f62f8e62b7baebf9e31
Carl <carlgoshert@gmail.com>
2017-12-05 10:11:48 -0600

    Altered test to reflect changes to view. Passed.

--- tests/Feature/UserCreatePageTest.php
+++ tests/Feature/UserCreatePageTest.php
@@ -21,10 +21,6 @@ class UserCreatePageTest extends TestCase
             $response->assertSee('<input type="email" class="form-control" id="email" name="email" required>');
             $response->assertSee('<label for="role" class="col-md-2 control-label">Role:</label>');
             $response->assertSee('<input type="text" class="form-control" id="role" name="role" required>');
-            $response->assertSee('<label for="password" class="col-md-2 control-label">Password</label>');
-            $response->assertSee('<input id="password" type="password" class="form-control" name="password">');
-            $response->assertSee('<label for="password_confirmation" class="col-md-2 control-label">Confirm Password:</label>');
-            $response->assertSee('<input id="password_confirmation" type="password" class="form-control" name="password_confirmation">');
             $response->assertSee('<a class="btn btn-default" href="http://localhost">Cancel</a>');
             $response->assertSee('<button type="submit" class="btn btn-primary">Submit</button>');
     }
