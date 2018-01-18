ec00f6a09b70360ff40473b842cbb002ff7192eb
kschick02 <kschick02@aurora.edu>
2018-01-18 11:19:04 -0600

    added factories for lab/order tables

---
+++ database/factories/LabFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Patient;
+use Faker\Generator as Faker;
+
+/* @var Illuminate\Database\Eloquent\Factory $factory */
+
+$factory->define(App\Lab::class, function (Faker $faker) {
+    $name = $faker->words(2, true);
+    
+    return [
+      'name' => $name,
+      'description' => $faker->sentence(6, true),
+      'file_path' => 'labs/' . $name . rand(1111, 9999) . '.pdf',
+      'patient_id' => App\Patient::pluck('medical_record_number')->random(),
+    ];
+});
