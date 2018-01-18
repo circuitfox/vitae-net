c06949a3b7bd92df43fcf2df3821eedb893eba63
kschick02 <kschick02@aurora.edu>
2018-01-18 13:55:53 -0600

    patient_id field/styling fixed

--- database/factories/LabFactory.php
+++ database/factories/LabFactory.php
@@ -6,12 +6,13 @@ use Faker\Generator as Faker;
 /* @var Illuminate\Database\Eloquent\Factory $factory */
 
 $factory->define(App\Lab::class, function (Faker $faker) {
-    $name = $faker->words(2, true);
-    
     return [
+      $name = $faker->words(2, true);
       'name' => $name,
       'description' => $faker->sentence(6, true),
       'file_path' => 'labs/' . $name . rand(1111, 9999) . '.pdf',
-      'patient_id' => App\Patient::pluck('medical_record_number')->random(),
+      'patient_id' => function() {
+          return factory(App\Patient::class)->create()->medical_record_number;
+      },
     ];
 });
--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -5,14 +5,14 @@ use Faker\Generator as Faker;
 /* @var Illuminate\Database\Eloquent\Factory $factory */
 
 $factory->define(App\Order::class, function (Faker $faker) {
-
-  $name = $faker->words(2, true);
     return [
+        $name = $faker->words(2, true);
         'name' => $name,
         'description' => $faker->sentence(6, true),
         'file_path' => 'orders/' . $name . rand(1111, 9999) . '.pdf',
-        'patient_id' => App\Patient::pluck('medical_record_number')->random(),
+        'patient_id' => function() {
+            return factory(App\Patient::class)->create()->medical_record_number;
+        },
         'completed' => $faker->boolean,
-
     ];
 });
