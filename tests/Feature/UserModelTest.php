215a266141d9af38a65b523361962aec48c57624
Chris Stankus <cdstankus@gmail.com>
2018-02-04 12:48:18 -0600

    Remove DatabaseMigrations from model tests
    The only tests that need DatabaseMigrations are the migration tests.
    Everything else can use the faster RefreshDatabase.

--- tests/Feature/UserModelTest.php
+++ tests/Feature/UserModelTest.php
@@ -3,13 +3,11 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class UserModelTest extends TestCase
 {
-    use DatabaseMigrations;
+    use RefreshDatabase;
 
     public function testFactory()
     {
