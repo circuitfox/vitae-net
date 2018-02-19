0b58d1f17bf32f331e3374dbd33c982a877ec60b
Chris Stankus <cdstankus@gmail.com>
2018-02-19 11:03:56 -0600

    Add install for git 2.9 in centos

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -3,8 +3,8 @@ set -ev
 
 # install dependencies
 yum install -y centos-release-scl.noarch epel-release.noarch
-yum install -y rh-php71 rh-php71-php-cli rh-php71-php-fpm rh-php71-php-json rh-php71-php-mbstring rh-php71-php-mysqlnd rh-php71-php-xml
-
+yum install -y rh-git29 rh-php71 rh-php71-php-cli rh-php71-php-fpm rh-php71-php-json rh-php71-php-mbstring rh-php71-php-mysqlnd rh-php71-php-xml
+scl enable rh-git29 bash
 # symlink php
 if [ ! -e /usr/bin/php ]; then
     ln -s /opt/rh/rh-php71/root/bin/php /usr/bin/php
