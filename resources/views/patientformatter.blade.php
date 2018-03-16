32d93e9981336244d8cfdc746478e7e1529be34b
Chris Stankus <cdstankus@gmail.com>
2018-03-16 09:56:59 -0500

    Clean up QR format layouts

--- resources/views/patientformatter.blade.php
+++ resources/views/patientformatter.blade.php
@@ -5,41 +5,62 @@
   <div class="panel panel-default" id="panel">
     <div class="panel-heading">Patient Data Formatter</div>
     <div class="panel-body">
-      <div class="col-sm-4">
-        <label for="mrn" class="pull-left">MRN:</label>
-        <input type="text" id="mrn" name="mrn" class="pull-right" /><br /><br />
-        <label for="lname" class="pull-left">Last Name:</label>
-        <input type="text" id="lname" name="lname" class="pull-right" /><br /><br />
-        <label for="fname" class="pull-left">First Name:</label>
-        <input type="text" id="fname" name="fname" class="pull-right" /><br /><br />
-        <label for="dob" class="pull-left">Date of Birth:</label>
-        <input type="text" id="dob" name="dob" class="pull-right" /><br /><br />
-        <label for="sex" class="pull-left">Sex:</label>
-        <input type="text" id="sex" name="sex" class="pull-right" /><br /><br />
-        <label for="height" class="pull-left">Height:</label>
-        <input type="text" id="height" name="height" class="pull-right" /><br /><br />
-        <label for="weight" class="pull-left">Weight:</label>
-        <input type="text" id="weight" name="weight" class="pull-right" /><br /><br />
-        <label for="diagnosis" class="pull-left">Diagnosis:</label>
-        <input type="text" id="diagnosis" name="diagnosis" class="pull-right" /><br /><br />
-        <label for="allergies" class="pull-left">Allergies:</label>
-        <input type="text" id="allergies" name="allergies" class="pull-right" /><br /><br />
-        <label for="code" class="pull-left">Code Status:</label>
-        <input type="text" id="code" name="code" class="pull-right" /><br /><br />
-        <label for="phsyician" class="pull-left">Physician:</label>
-        <input type="text" id="physician" name="physician" class="pull-right" /><br /><br />
-        <label for="room" class="pull-left">Room:</label>
-        <input type="text" id="room" name="room" class="pull-right" />
-      </div>
-      <div class="col-sm-8">
-        <button type="button" id="patient-format" class="pull-right">Format</button>
-        <br />
-        <br />
-        <p class="pull-right">Formatted code data:</p>
-        <br />
-        <p id="output" class="pull-right"></p>
-        <br />
-      </div>
+      <form class="form-horizontal">
+        <div class="col-sm-6">
+          <div class="form-group">
+            <label for="mrn">MRN:</label>
+            <input type="text" id="mrn" name="mrn">
+          </div>
+          <div class="form-group">
+            <label for="lname">Last Name:</label>
+            <input type="text" id="lname" name="lname">
+          </div>
+          <div class="form-group">
+            <label for="fname">First Name:</label>
+            <input type="text" id="fname" name="fname">
+          </div>
+          <div class="form-group">
+            <label for="dob">Date of Birth:</label>
+            <input type="text" id="dob" name="dob">
+          </div>
+          <div class="form-group">
+            <label for="sex">Sex:</label>
+            <input type="text" id="sex" name="sex">
+          </div>
+          <div class="form-group">
+            <label for="height">Height:</label>
+            <input type="text" id="height" name="height">
+          </div>
+          <div class="form-group">
+            <label for="weight">Weight:</label>
+            <input type="text" id="weight" name="weight">
+          </div>
+          <div class="form-group">
+            <label for="diagnosis">Diagnosis:</label>
+            <input type="text" id="diagnosis" name="diagnosis">
+          </div>
+          <div class="form-group">
+            <label for="allergies">Allergies:</label>
+            <input type="text" id="allergies" name="allergies">
+          </div>
+          <div class="form-group">
+            <label for="code">Code Status:</label>
+            <input type="text" id="code" name="code">
+          </div>
+          <div class="form-group">
+            <label for="phsyician">Physician:</label>
+            <input type="text" id="physician" name="physician">
+          </div>
+          <div class="form-group">
+            <label for="room">Room:</label>
+            <input type="text" id="room" name="room">
+          </div>
+        </div>
+      </form>
+      <div class="col-sm-6">
+          <button type="button" id="patient-format" class="btn btn-default">Format</button>
+          <p>Formatted code data:</p>
+          <p id="output"></p>
       </div>
     </div>
   </div>
