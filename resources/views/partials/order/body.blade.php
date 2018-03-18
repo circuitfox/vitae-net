9cba85f9291e9b0871cfbcff9729022d9fa7d554
jameson-beretta <laxsuperstar1@gmail.com>
2018-03-18 13:57:37 -0500

    Order Complete Button

--- resources/views/partials/order/body.blade.php
+++ resources/views/partials/order/body.blade.php
@@ -1,11 +1,16 @@
-<div class="col-sm-3">
-  <!-- order info -->
-  <h5><b><u>Name:</u></b></h5>
-  <p>{{ $order->name }}</p>
-  <h5><b><u>Description:</u></b></h5>
-  <p>{{ $order->description }}</p>
-  <h5><b><u>Patient MRN:</u></b></h5>
-  <p>{{ $order->patient_id }}</p>
-  <h5><b><u>Completed:</u></b></h5>
-  <p>{{ $order->completed ? 'Yes' : 'No' }}</p>
+<div class="container">
+  <div class="col-sm-4">
+    <!-- order info -->
+    <h5><b><u>Name:</u></b></h5>
+    <p>{{ $order->name }}</p>
+    <h5><b><u>Description:</u></b></h5>
+    <p>{{ $order->description }}</p>
+    <h5><b><u>Patient MRN:</u></b></h5>
+    <p>{{ $order->patient_id }}</p>
+    <h5><b><u>Completed:</u></b></h5>
+    <p>{{ $order->completed ? 'Yes' : 'No' }}</p>
+    <form method="POST" action="/orders/complete">
+      <button type="submit" class="btn btn-primary">Complete Order</button>
+    </form>
+  </div>
 </div>
