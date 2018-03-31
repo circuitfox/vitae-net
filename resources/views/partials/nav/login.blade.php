877c777758a89550e3ad5cb224c09e03ac9ba471
kschick02 <kschick02@aurora.edu>
2018-03-31 16:02:33 -0500

    replace navbar login

--- resources/views/partials/nav/login.blade.php
+++ resources/views/partials/nav/login.blade.php
@@ -1,4 +1,26 @@
 <div>
+@if (Auth::guest())
+  <form class="navbar-form navbar-right" id="login-form" action="{{ route('login') }}" method="POST">
+    {{ csrf_field() }}
+    <div class="form-group">
+      <input type="text" class="form-control" id="name" name="name" placeholder="Administrator Username">
+      @if ($errors->has('name'))
+        <span class="help-block">
+          <strong>{{ $errors->first('name') }}</strong>
+        </span>
+      @endif
+    </div>
+    <div class="form-group">
+      <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+      @if ($errors->has('password'))
+        <span class="help-block">
+          <strong>{{ $errors->first('password') }}</strong>
+        </span>
+      @endif
+    </div>
+    <button type="submit" class="btn btn-default">Sign in</button>
+  </form>
+@else
   <div class="navbar-right">
     <div class="navbar-text">
       <span class="glyphicon glyphicon-cog"></span>
@@ -14,4 +36,5 @@
       <a class="navbar-link" href="{{ url('/logout') }}">Logout</a>
     </div>
   </div>
+@endif
 </div>
