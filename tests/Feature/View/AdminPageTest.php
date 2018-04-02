76150ae37f23e1b4f0a35fdad5eb898970ab09dd
Carl <carlgoshert@gmail.com>
2018-04-02 10:13:34 -0500

    Altered assertions for instructor users.

--- tests/Feature/View/AdminPageTest.php
+++ tests/Feature/View/AdminPageTest.php
@@ -9,7 +9,7 @@ class AdminPageTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function testHasPanel()
+    public function testHasPanelIfAdmin()
     {
         $user = factory(\App\User::class)->states('admin')->create();
         $users = url('/users');
@@ -25,6 +25,34 @@ class AdminPageTest extends TestCase
         $response->assertSee('<p>Administrator options:</p>');
         $response->assertSee('<p><a class="btn btn-primary" href="' . url('/users') . '">View Users</a>&nbsp;View and edit existing users.</p>');
         $response->assertSee('<p><a class="btn btn-primary" href="' . url('/users/create') . '">Create User</a>&nbsp;Create new user.</p>');
+        $response->assertSee('<p><a class="btn btn-primary" href="' . url('/medications') . '">View Medications</a>&nbsp;View and edit existing medications.</p>');
+        $response->assertSee('<p><a class="btn btn-primary" href="' . url('/medications/create') . '">Create Medication</a>&nbsp;Add new medication.</p>');
+        $response->assertSee('<p><a class="btn btn-primary" href="' . url('/medformatter') . '">Format Medication</a>&nbsp;Format medication data for QR/bar code.</p>');
+        $response->assertSee('<p><a class="btn btn-primary" href="' . url('/patients') . '">View Patients</a>&nbsp;View and edit existing patients.</p>');
+        $response->assertSee('<p><a class="btn btn-primary" href="' . url('/patients/create') . '">Create Patient</a>&nbsp;Create new patient.</p>');
+        $response->assertSee('<p><a class="btn btn-primary" href="' . url('/patientformatter') . '">Format Patient</a>&nbsp;Format patient data for QR/bar code.</p>');
+        $response->assertSee('<p><a class="btn btn-primary" href="' . url('/orders') . '">View Orders</a>&nbsp;View and edit existing orders.</p>');
+        $response->assertSee('<p><a class="btn btn-primary" href="' . url('/orders/create') . '">Create Order</a>&nbsp;Create new order.</p>');
+        $response->assertSee('<p><a class="btn btn-primary" href="' . url('/labs') . '">View Labs</a>&nbsp;View and edit existing labs.</p>');
+        $response->assertSee('<p><a class="btn btn-primary" href="' . url('/labs/create') . '">Create Lab</a>&nbsp;Create new lab.</p>');
+    }
+
+    public function testHasPanelIfInstructor()
+    {
+        $user = factory(\App\User::class)->states('instructor')->create();
+        $users = url('/users');
+        $userscreate = url('/users/create');
+        $meds = url('/medications');
+        $medscreate = url('/medications/create');
+        $medformat = url('/medformatter');
+        $patients = url('/patients');
+        $patientscreate = url('/patients/create');
+        $patientformat = url('/patientformatter');
+        $response = $this->actingAs($user)->get('/home');
+        $response->assertSee('<div class="panel-heading">Administrator Dashboard</div>');
+        $response->assertSee('<p>Administrator options:</p>');
+        $response->assertDontSee('<p><a class="btn btn-primary" href="' . url('/users') . '">View Users</a>&nbsp;View and edit existing users.</p>');
+        $response->assertDontSee('<p><a class="btn btn-primary" href="' . url('/users/create') . '">Create User</a>&nbsp;Create new user.</p>');
         $response->assertSee('<p><a class="btn btn-primary" href="' . url('/medications') . '">View Medications</a>&nbsp;View and edit existing medications.</p>');
         $response->assertSee('<p><a class="btn btn-primary" href="' . url('/medications/create') . '">Create Medication</a>&nbsp;Add new medication.</p>');
         $response->assertSee('<p><a class="btn btn-primary" href="' . url('/medformatter') . '">Format Medication</a>&nbsp;Format medication data for QR/bar code.</p>');
