afc06fbd20e256fa73e4c34ab8ff02b3c786cb26
Carl Goshert <carlgoshert@gmail.com>
2018-04-01 22:29:46 -0500

    Gave instructors access to admin dashboard.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,7 +14,7 @@ class HomeController extends Controller
 
     public function index()
     {
-        if (Auth::user()->isAdmin()) {
+        if (Auth::user()->isAdmin() || Auth::user()->isInstructor()) {
             return view('admin');
         } else {
             return redirect('/patients');
