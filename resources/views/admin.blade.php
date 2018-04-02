c1cc50aad0cc274513dfa8d1d12ef1bf5f2527fd
Chris Stankus <cdstankus@gmail.com>
2018-04-02 10:15:51 -0500

    Reformat admin page

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,7 +1,7 @@
 @extends("layouts.app")
 @section("title", "Vitae NET Admin Page")
 @section("content")
-<div class="col-panel">
+<div class="container col-panel">
   <div class="panel panel-default" id="panel">
     <div class="panel-heading">Administrator Dashboard</div>
     <div class="panel-body">
@@ -10,20 +10,94 @@
           <span class="text-center"><strong>Admin:</strong> Remember to change your email address!</span>
         </div>
       @endif
-      <p>Administrator options:</p>
-      <p><a class="btn btn-primary" href="{{ url('/users') }}">View Users</a>&nbsp;View and edit existing users.</p>
-      <p><a class="btn btn-primary" href="{{ url('/users/create') }}">Create User</a>&nbsp;Create new user.</p>
-      <p><a class="btn btn-primary" href="{{ url('/signatures') }}">View Signatures</a>&nbsp;View student medication administration signatures</p>
-      <p><a class="btn btn-primary" href="{{ url('/medications') }}">View Medications</a>&nbsp;View and edit existing medications.</p>
-      <p><a class="btn btn-primary" href="{{ url('/medications/create') }}">Create Medication</a>&nbsp;Add new medication.</p>
-      <p><a class="btn btn-primary" href="{{ url('/medformatter') }}">Format Medication</a>&nbsp;Format medication data for QR/bar code.</p>
-      <p><a class="btn btn-primary" href="{{ url('/patients') }}">View Patients</a>&nbsp;View and edit existing patients.</p>
-      <p><a class="btn btn-primary" href="{{ url('/patients/create') }}">Create Patient</a>&nbsp;Create new patient.</p>
-      <p><a class="btn btn-primary" href="{{ url('/patientformatter') }}">Format Patient</a>&nbsp;Format patient data for QR/bar code.</p>
-      <p><a class="btn btn-primary" href="{{ url('/orders') }}">View Orders</a>&nbsp;View and edit existing orders.</p>
-      <p><a class="btn btn-primary" href="{{ url('/orders/create') }}">Create Order</a>&nbsp;Create new order.</p>
-      <p><a class="btn btn-primary" href="{{ url('/labs') }}">View Labs</a>&nbsp;View and edit existing labs.</p>
-      <p><a class="btn btn-primary" href="{{ url('/labs/create') }}">Create Lab</a>&nbsp;Create new lab.</p>
+      <h2>Users:</h2>
+      <hr>
+      <div class="row">
+        <div class="col-md-6">
+          <a class="btn btn-primary" href="{{ url('/users') }}">View Users</a>
+          <h4>View and edit existing users.</h4>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6">
+          <a class="btn btn-primary" href="{{ url('/users/create') }}">Create User</a>
+          <h4>Create new user.</h4>
+        </div>
+      </div>
+      <h2>Medications:</h2>
+      <hr>
+      <div class="row">
+        <div class="col-md-6">
+          <a class="btn btn-primary" href="{{ url('/signatures') }}">View Signatures</a>
+          <h4>View student medication administration signatures.</h4>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6">
+          <a class="btn btn-primary" href="{{ url('/medications') }}">View Medications</a>
+          <h4>View and edit existing medications.</h4>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6">
+          <a class="btn btn-primary" href="{{ url('/medications/create') }}">Create Medication</a>
+          <h4>Add new medication.</h4>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6">
+          <a class="btn btn-primary" href="{{ url('/medformatter') }}">Format Medication</a>
+          <h4>Format medication data for QR codes.</h4>
+        </div>
+      </div>
+      <h2>Patients:</h2>
+      <hr>
+      <div class="row">
+        <div class="col-md-6">
+          <a class="btn btn-primary" href="{{ url('/patients') }}">View Patients</a>
+          <h4>View and edit existing patients.</h4>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6">
+          <a class="btn btn-primary" href="{{ url('/patients/create') }}">Create Patient</a>
+          <h4>Create new patient.</h4>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6">
+          <a class="btn btn-primary" href="{{ url('/patientformatter') }}">Format Patient</a>
+          <h4>Format patient data for QR codes.</h4>
+        </div>
+      </div>
+      <h2>Orders:</h2>
+      <hr>
+      <div class="row">
+        <div class="col-md-6">
+          <a class="btn btn-primary" href="{{ url('/orders') }}">View Orders</a>
+          <h4>View and edit existing orders.</h4>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6">
+          <a class="btn btn-primary" href="{{ url('/orders/create') }}">Create Order</a>
+          <h4>Create new order.</h4>
+        </div>
+      </div>
+      <h2>Labs:</h2>
+      <hr>
+      <div class="row">
+        <div class="col-md-6">
+          <a class="btn btn-primary" href="{{ url('/labs') }}">View Labs</a>
+          <h4>View and edit existing labs.</h4>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6">
+          <a class="btn btn-primary" href="{{ url('/labs/create') }}">Create Lab</a>
+          <h4>Create new lab.</h4>
+        </div>
+      </div>
     </div>
   </div>
 </div>
