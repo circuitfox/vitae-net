93be18195a75a5bfdc68fd9640d398335303ac6e
Carl <carlgoshert@gmail.com>
2018-04-23 09:57:27 -0500

    Updated assessments to match requested changes.

--- tests/Feature/Table/CreateAssessmentsTableTest.php
+++ tests/Feature/Table/CreateAssessmentsTableTest.php
@@ -122,9 +122,29 @@ class CreateAssessmentsTableTest extends TestCase
         $this->assertTrue(Schema::hasColumn('assessments', 'pupillary'));
     }
 
-    public function testHasPainColumn()
+    public function testHasPupilSizeColumn()
     {
-        $this->assertTrue(Schema::hasColumn('assessments', 'pain'));
+        $this->assertTrue(Schema::hasColumn('assessments', 'pupil_size'));
+    }
+
+    public function testHasPupilShapeColumn()
+    {
+        $this->assertTrue(Schema::hasColumn('assessments', 'pupil_shape'));
+    }
+
+    public function testHasAccommodationColumn()
+    {
+        $this->assertTrue(Schema::hasColumn('assessments', 'accommodation'));
+    }
+
+    public function testHasPainScaleColumn()
+    {
+        $this->assertTrue(Schema::hasColumn('assessments', 'pain_scale'));
+    }
+
+    public function testHasPainLocationColumn()
+    {
+        $this->assertTrue(Schema::hasColumn('assessments', 'pain_location'));
     }
 
     public function testHasSkincolorColumn()
@@ -177,14 +197,24 @@ class CreateAssessmentsTableTest extends TestCase
         $this->assertTrue(Schema::hasColumn('assessments', 'capillary'));
     }
 
-    public function testHasUpperColumn()
+    public function testHasRightUpperColumn()
     {
-        $this->assertTrue(Schema::hasColumn('assessments', 'upper'));
+        $this->assertTrue(Schema::hasColumn('assessments', 'right_upper'));
     }
 
-    public function testHasBreathrhythmColumn()
+    public function testHasLeftUpperColumn()
     {
-        $this->assertTrue(Schema::hasColumn('assessments', 'breathrhythm'));
+        $this->assertTrue(Schema::hasColumn('assessments', 'left_upper'));
+    }
+
+    public function testHasRightBreathColumn()
+    {
+        $this->assertTrue(Schema::hasColumn('assessments', 'right_breath'));
+    }
+
+    public function testHasLeftBreathColumn()
+    {
+        $this->assertTrue(Schema::hasColumn('assessments', 'left_breath'));
     }
 
     public function testHasCoughColumn()
@@ -197,14 +227,19 @@ class CreateAssessmentsTableTest extends TestCase
         $this->assertTrue(Schema::hasColumn('assessments', 'secretions'));
     }
 
-    public function testHasRoomairColumn()
+    public function testHasSupplementalColumn()
     {
-        $this->assertTrue(Schema::hasColumn('assessments', 'roomair'));
+        $this->assertTrue(Schema::hasColumn('assessments', 'supplemental'));
     }
 
-    public function testHasNauseaColumn()
+    public function testHasLitersPerMinuteColumn()
     {
-        $this->assertTrue(Schema::hasColumn('assessments', 'nausea'));
+        $this->assertTrue(Schema::hasColumn('assessments', 'liters_per_minute'));
+    }
+
+    public function testHasDietColumn()
+    {
+        $this->assertTrue(Schema::hasColumn('assessments', 'diet'));
     }
 
     public function testHasAbdomenColumn()
@@ -242,14 +277,24 @@ class CreateAssessmentsTableTest extends TestCase
         $this->assertTrue(Schema::hasColumn('assessments', 'muscle'));
     }
 
-    public function testHasPedalColumn()
+    public function testHasRightPedalColumn()
     {
-        $this->assertTrue(Schema::hasColumn('assessments', 'pedal'));
+        $this->assertTrue(Schema::hasColumn('assessments', 'right_pedal'));
     }
 
-    public function testHasLowerColumn()
+    public function testHasLeftPedalColumn()
     {
-        $this->assertTrue(Schema::hasColumn('assessments', 'lower'));
+        $this->assertTrue(Schema::hasColumn('assessments', 'left_pedal'));
+    }
+
+    public function testHasRightLowerColumn()
+    {
+        $this->assertTrue(Schema::hasColumn('assessments', 'right_lower'));
+    }
+
+    public function testHasLeftLowerColumn()
+    {
+        $this->assertTrue(Schema::hasColumn('assessments', 'left_lower'));
     }
 
     public function testHasPeripheralColumn()
@@ -257,14 +302,14 @@ class CreateAssessmentsTableTest extends TestCase
         $this->assertTrue(Schema::hasColumn('assessments', 'peripheral'));
     }
 
+    public function testHasCalfColumn()
+    {
+        $this->assertTrue(Schema::hasColumn('assessments', 'calf'));
+    }
+
     public function testHasTedColumn()
     {
         $this->assertTrue(Schema::hasColumn('assessments', 'ted'));
-    }
-
-    public function testHasRestraintsColumn()
-    {
-        $this->assertTrue(Schema::hasColumn('assessments', 'restraints'));
     }
 
     public function testHasDrainageColumn()
